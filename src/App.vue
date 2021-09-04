27a99f0de4319a33029e31de91f4e68c0113aed3
yagmour <yagmour@protonmail.com>
2021-09-04 00:40:49 +0000

    Add vue-sidebar-menu & bootstrap.

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,10 @@
 <template>
-  <h1>Awesome portfolio under construction!</h1>
+  <div class="container">
+    <div class="col">
+        <sidebar-menu :menu="menu"/>
+    </div>
+    <div class="col">
+        <h1>Awesome portfolio under construction!</h1>
+    </div>
+  </div>
 </template>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,12 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import 'bootstrap'
+import 'bootstrap/dist/css/bootstrap.css'
+import VueSideBarMenu from 'vue-sidebar-menu'
+import 'vue-sidebar-menu/dist/vue-sidebar-menu.css'
 
-createApp(App).mount('#app')
+// createApp(App).mount('#app')
+
+const app = createApp(App)
+app.use(VueSideBarMenu)
+app.mount('#app')
