070550a193ec99e1664ef42a9014cc6e028d9274
yagmour <yagmour@protonmail.com>
2021-09-14 14:56:23 +0000

    Refactor tabmenu 'active' styling.
    Removed all javascript which computed which tab was 'active'. Same
    effect is produced by instead using the .router-link-active class.

--- src/components/tabmenu/Tabmenu.vue
+++ src/components/tabmenu/Tabmenu.vue
@@ -2,10 +2,10 @@
 
 <div class="container">
   <div class="row justify-content-center" style="border-bottom: solid var(--btn-light)">
-    <router-link :to="to_code" class="col-sm-2 slanted-left text-white routerlink" :class="{current: isCodeActive}">
+    <router-link :to="to_code" class="col-sm-2 slanted-left text-white routerlink">
       <h2 class="text-center link-bottom">Code</h2>
     </router-link>
-    <router-link :to="to_journal" class="col-sm-2 slanted-right text-white routerlink" :class="{current: isJournalActive}">
+    <router-link :to="to_journal" class="col-sm-2 slanted-right text-white routerlink">
       <h2 class="text-center link-bottom">Journal</h2>
     </router-link>
   </div>
@@ -14,19 +14,10 @@
 </template>
 
 <script>
-import {computed} from 'vue'
-import {useRoute} from 'vue-router'
-
 export default {
   props: {
     to_code: {type: String, required: true},
     to_journal: {type: String, required: true}
-  },
-  setup(props) {
-    const route = useRoute()
-    const isCodeActive = computed(()=> route.path === props.to_code)
-    const isJournalActive = computed(()=> route.path === props.to_journal)
-    return {isCodeActive, isJournalActive}
   }
 }
 </script>
@@ -38,17 +29,13 @@ export default {
   clip-path: polygon(8% 0, 100% 0, 100% 100%, 0 100%, 0 24%);
 }
 
-.slanted-left:focus {
-  background-color: #b35900;
-}
-
 .slanted-right {
   background-color: var(--btn-light);
   -webkit-clip-path: polygon(92% 0, 100% 24%, 100% 100%, 0 100%, 0 0%);
   clip-path: polygon(92% 0, 100% 24%, 100% 100%, 0 100%, 0 0%);
 }
 
-.slanted-right:focus {
+.router-link-active {
   background-color: var(--orange);
 }
 
