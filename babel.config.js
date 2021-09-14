acd2ad52aba08794c47946a5bfc53c3e2d9bf745
yagmour <yagmour@protonmail.com>
2021-09-14 23:00:54 +0000

    Removed redundant and erroneous code.

--- babel.config.js
+++ babel.config.js
@@ -1,13 +1,5 @@
 module.exports = {
   presets: [
     '@vue/cli-plugin-babel/preset'
-  ],
-  plugins: [
-    rules: [
-      {
-        test: /\.md$/i,
-        use: 'raw-loader',
-      }
-    ]
   ]
 }
